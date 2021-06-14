0945e2f4b88f04d730efab99d3d744716b9b4eb9
yen102 <71604562+yen102@users.noreply.github.com>
2021-06-14 08:00:15 +0700

    Add files via upload

---
+++ WhoWroteIt/java/hust/soict/whowroteit/MainActivity.java
@@ -0,0 +1,61 @@
+package hust.soict.whowroteit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = (EditText) findViewById(R.id.book_input);
+        mTitleText = (TextView) findViewById(R.id.titleText);
+        mAuthorText = (TextView) findViewById(R.id.authorText);
+
+
+    }
+
+    public void searchBook(View view) {
+        String queryString = mBookInput.getText().toString();
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS
+            );
+        }
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+
+    }
+
+}
