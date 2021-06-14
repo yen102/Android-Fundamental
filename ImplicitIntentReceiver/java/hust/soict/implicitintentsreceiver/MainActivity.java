9e8726ac84ad2baa3156ee5cedeacc6582266bd2
yen102 <71604562+yen102@users.noreply.github.com>
2021-06-14 08:17:54 +0700

    Add files via upload

---
+++ ImplicitIntentReceiver/java/hust/soict/implicitintentsreceiver/MainActivity.java
@@ -0,0 +1,25 @@
+package hust.soict.implicitintentsreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if(uri != null) {
+            String uri_string = getString(R.string.uri_lable) + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
+    }
+}
